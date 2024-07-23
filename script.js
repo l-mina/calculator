1417189763fcb0003027a73c9e1de1b05b547f9d
Luis M <150566024+l-mina@users.noreply.github.com>
2024-07-23 19:47:50 -0400

    User inputs updates the HTML text

--- script.js
+++ script.js
@@ -17,32 +17,77 @@ function divide(a,b){
     return a / b;
 }
 
+function total(displayStr){
+        let a = '';
+        let b = '';
+        let b_val = false;
+        let op_index = null;
+        for(let i = 0; i < displayStr.length; i++){
+            if( displayStr[i] == '+' ||
+                displayStr[i] == '-' ||
+                displayStr[i] == '*' ||
+                displayStr[i] == '/'){
+                    b_val = true;
+                    op_index = i;
+            } else if(!b_val){
+                a+=displayStr[i];
+            } else {
+                b+=displayStr[i];
+            }
+        }
+        alert(operate(parseInt(a),displayStr[op_index],parseInt(b)));
+        
+}
+
 //Calculator key functions
 function operate(a,op,b){
     switch(op){
         case '+':
-            add(a,b);
+            return add(a,b);
             break;
         case '-':
-            subtract(a,b);
+            return subtract(a,b);
             break;
         case '*':
-            multiply(a,b);
+            return multiply(a,b);
             break;
         case '/':
-            divide(a,b);
+            return divide(a,b);
             break;  
     }
 }
 
 //Main
-/*
-const digitButton = document.getElementById("digit");
-digitButton.addEventListener("click",()=>{
-    alert(digitButton.innerText);
-})
-*/
-const digitButton = document.querySelector(".digit");
-digitButton.forEach(addEventListener("click",(event)=>{
-    alert(digitButton.innerHTML);
-}))
+
+const display = document.querySelector(".display");
+
+const digits = document.querySelectorAll(".digit");
+
+const operator = document.querySelectorAll(".operator");
+
+const equal = document.querySelector(".equal");
+
+let displayStr = '';
+
+digits.forEach(btn => {
+    btn.addEventListener('click', event => {
+        let num = event.target.innerHTML;
+        displayStr += num;
+        display.innerText = displayStr;
+   });
+
+});
+
+operator.forEach(btn => {
+    btn.addEventListener('click', event => {
+        let num = event.target.innerHTML;
+        displayStr += num;
+        display.innerText = displayStr;
+   });
+
+});
+
+equal.addEventListener('click', ()=>{
+    total(displayStr);
+});
+
